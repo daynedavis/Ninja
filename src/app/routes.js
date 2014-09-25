42c6f5bd6cbe19c59ccd3701364a66e40be4e4d4
Dayne Davis <daynedavis5@gmail.com>
2014-09-25 12:54:37 -0400

    Beautify files

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,17 +1,19 @@
 angular.module("ninja.routes", ["ui.router"])
 
-.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
-  $urlRouterProvider.otherwise('/');
-  $locationProvider.html5Mode(true);
-  $stateProvider
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+  function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
+    $stateProvider
 
-  .state("home", {
-    url: "/",
-    views: {
-      "main@": {
-        controller: "MainController",
-        templateUrl: "app/templates/layouts/home.tpl.html"
+    .state("home", {
+      url: "/",
+      views: {
+        "main@": {
+          controller: "MainController",
+          templateUrl: "app/templates/layouts/home.tpl.html"
+        }
       }
-    }
-  });
-}]);
+    });
+  }
+]);
