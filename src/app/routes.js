4dadaf3b27a40eede3d910c4b404aad20078299a
Dayne Davis <daynedavis5@gmail.com>
2014-09-24 23:04:28 -0400

    removed # in url with $locationProvider

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,7 +1,8 @@
 angular.module("ninja.routes", ["ui.router"])
 
-.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
   $urlRouterProvider.otherwise('/');
+  $locationProvider.html5Mode(true);
   $stateProvider
 
   .state("home", {
